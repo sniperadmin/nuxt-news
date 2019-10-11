6a40d45992fc4b4ab6a362651f06487d2b253f52
Nasr Galal <nasrbue@gmail.com>
2019-10-11 09:03:11 +0200

    created the app with contents

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="bg-info text-white pt-1">
+      <div class="d-flex justify-content-between container">
+        <h3>
+          PWA News
+        </h3>
+        <h2 class="subtitle">
+          All the headlines making the wavy!
+        </h2>
+      </div>
+    </div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+  html {
+    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+      Roboto, 'Helvetica Neue', Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container mt-5">
+    <div class="container">
+      <b-row>
+        <b-col v-for="(article, index) in articles" :key="index" md="4">
+          <b-card
+            :title="article.title"
+            :img-src="article.urlToImage"
+            :img-alt="article.title"
+            img-top
+            style="max-width: 20rem;"
+            class="mb-2"
+          >
+            <b-card-text>
+              {{ article.description }}
+            </b-card-text>
+
+            <b-button :href="article.url">
+              more info
+            </b-button>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({
+    app
+  }) {
+    const {
+      articles
+    } = await app.$axios.$get(
+      `https://newsapi.org/v2/top-headlines?language=ar&apiKey=${process.env.API_KEY}`
+    )
+    return {
+      articles
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
